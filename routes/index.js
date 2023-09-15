1466b89b6ff029fef88e8d3a36eb1494c2b3f12a
Cyndowow <71725217+Cyndowow@users.noreply.github.com>
2023-09-15 11:26:46 +0200

    Add auth routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const Message = require("../models/message");
+
+const auth_controller = require("../controllers/auth");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
+
+// Auth routes
+router.get("/log-in", auth_controller.log_in_get);
+
+router.post("/log-in", auth_controller.log_in_post);
+
+router.get("/log-out", auth_controller.log_out);
+
+router.get("/sign-up", auth_controller.sign_up_get);
+
+router.post("/sign-up", auth_controller.sign_up_post);
 
 module.exports = router;
