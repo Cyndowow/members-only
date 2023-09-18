e1a9dd89c1c175a2dc699987c922e731f5863e97
Cyndowow <71725217+Cyndowow@users.noreply.github.com>
2023-09-18 11:16:35 +0200

    Add middleware functions

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,46 @@ var router = express.Router();
 const Message = require("../models/message");
 
 const auth_controller = require("../controllers/auth");
+const message_controller = require("../controllers/message");
+
+//check if user is logged in
+function isLoggedIn(req, res, next) {
+  if (req.user) {
+    next();
+  } else {
+    res.redirect("/log-in");
+  }
+}
+
+//check message-delete authentication
+function isAuthorizedDelete(req, res, next) {
+  Message.findOne({ _id: req.params.id })
+    .populate("User")
+    .then((messageOwner) => {
+      const isOwner =
+        JSON.stringify(messageOwner.user._id) === JSON.stringify(req.user.id);
+      if (isOwner || req.user.admin) {
+        next();
+      } else {
+        console.log("Not authorized to delete message.");
+        res.redirect("/");
+      }
+    })
+    .catch((err) => next(err));
+}
+
+//check if user is admin
+function isAdmin(req, res, next) {
+  if (req.user.admin) {
+    next();
+  } else {
+    res.redirect("/");
+  }
+}
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Members Only" });
 });
 
 // Auth routes
